7ab11c0e246ca7ab20333c08fa61d7002180eff3
zhangteng-mark <zhangteng.mark@gmail.com>
2015-05-07 22:30:17 +0800

    done

--- part2/src/BaseballElimination.java
+++ part2/src/BaseballElimination.java
@@ -1,5 +1,6 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -10,7 +11,7 @@ import java.util.Map;
 public class BaseballElimination {
 
     private int n;
-    private Map<String,Integer> teams;
+    private Map<String, Integer> teams;
     private int[] wins;
     private int[] loss;
     private int[] left;
@@ -31,12 +32,14 @@ public class BaseballElimination {
         for (int i = 0; i < n; i++) {
             String line = in.readLine();
             String[] parts = line.split("\\s+");
-            teams.put(parts[0], i);
-            wins[i] = Integer.parseInt(parts[1]);
-            loss[i] = Integer.parseInt(parts[2]);
-            left[i] = Integer.parseInt(parts[3]);
+            int index = 0;
+            while (parts[index].trim().equals("")) index++;
+            teams.put(parts[index], i);
+            wins[i] = Integer.parseInt(parts[index + 1]);
+            loss[i] = Integer.parseInt(parts[index + 2]);
+            left[i] = Integer.parseInt(parts[index + 3]);
             for (int j = 0; j < n; j++) {
-                game[i][j] = Integer.parseInt(parts[4 + j]);
+                game[i][j] = Integer.parseInt(parts[4 + j + index]);
             }
         }
     }
@@ -81,6 +84,20 @@ public class BaseballElimination {
         checkTeamValidation(team);
         if (isTrivialEliminated(team)) return true;
 
+        FlowNetwork network = buildNetWork(team);
+        int S = teams.get(team);
+        int v = (n - 1) * (n - 2) / 2 + (n - 1) + 2;
+        int T = v - 1;
+        FordFulkerson fulkerson = new FordFulkerson(network, S, T);
+        for (FlowEdge edge : network.adj(S)) {
+            if (edge.flow() < edge.capacity()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private FlowNetwork buildNetWork(String team) {
         int v = (n - 1) * (n - 2) / 2 + (n - 1) + 2;
         FlowNetwork network = new FlowNetwork(v);
         int g = n - 1;
@@ -96,29 +113,48 @@ public class BaseballElimination {
                 network.addEdge(new FlowEdge(g, j, Double.POSITIVE_INFINITY));
             }
         }
-        FordFulkerson fulkerson = new FordFulkerson(network, S, T);
-        for (FlowEdge edge : network.adj(S)) {
-            if (edge.flow() < edge.capacity()) {
-                return true;
-            }
-        }
-        return false;
+        return network;
     }
 
     private boolean isTrivialEliminated(String team) {
+        return trivialEliminatedSet(team) != null;
+    }
+
+    private String trivialEliminatedSet(String team) {
         int teamIndex = teams.get(team);
         int totalWins = wins[teamIndex] + left[teamIndex];
-        for (int index : teams.values()) {
-            if (index == teamIndex) continue;
-            if (totalWins < wins[index]) return true;
+        for (String t : teams.keySet()) {
+            if (team.equals(t)) continue;
+            if (totalWins < wins[teams.get(t)]) return t;
         }
-        return false;
+        return null;
     }
 
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
         checkTeamValidation(team);
-        return null;
+        if (!isEliminated(team)) return null;
+
+        List<String> subset = new ArrayList<>();
+
+        if (isTrivialEliminated(team)) {
+            subset.add(trivialEliminatedSet(team));
+            return subset;
+        }
+
+        FlowNetwork network = buildNetWork(team);
+        int S = teams.get(team);
+        int v = (n - 1) * (n - 2) / 2 + (n - 1) + 2;
+        int T = v - 1;
+        FordFulkerson fulkerson = new FordFulkerson(network, S, T);
+
+        for (String t : teams()) {
+            if (!t.equals(team) && fulkerson.inCut(teams.get(t))) {
+                subset.add(t);
+            }
+        }
+
+        return subset;
     }
 
     private void checkTeamValidation(String team) {
@@ -132,6 +168,7 @@ public class BaseballElimination {
         BaseballElimination bbe = new BaseballElimination(args[0]);
         for (String t : bbe.teams()) {
             System.out.println(t + " " + bbe.isEliminated(t));
+            System.out.println(bbe.certificateOfElimination(t));
         }
     }
 
