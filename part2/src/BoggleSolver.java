06c1012cf4ceb3f4506517f47f1167680b99e511
zhangteng-mark <zhangteng.mark@gmail.com>
2015-05-09 21:36:48 +0800

    update

--- part2/src/BoggleSolver.java
+++ part2/src/BoggleSolver.java
@@ -5,10 +5,15 @@
  */
 public class BoggleSolver {
 
+    private TrieST<Integer> dict;
+
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
-
+        dict = new TrieST<>();
+        for (String w : dictionary) {
+            dict.put(w, 1);
+        }
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
@@ -19,7 +24,40 @@ public class BoggleSolver {
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
-        return 0;
+        if (!dict.contains(word)) return 0;
+        int len = word.length();
+        int score = 0;
+        switch (len) {
+            case 0:
+            case 1:
+            case 2:
+                score = 0; break;
+            case 3:
+            case 4:
+                score = 1; break;
+            case 5:
+                score = 2; break;
+            case 6:
+                score = 3; break;
+            case 7:
+                score = 5; break;
+            default:
+                score = 11; break;
+        }
+        return score;
+    }
+
+    public static void main(String[] args) {
+        In in = new In("dictionary-common.txt");
+        String[] dictionary = in.readAllStrings();
+        BoggleSolver solver = new BoggleSolver(dictionary);
+        System.out.println(solver.scoreOf("XXXXXXXXXZZZZZ"));
+        System.out.println(solver.scoreOf("A"));
+        System.out.println(solver.scoreOf("ABC"));
+        System.out.println(solver.scoreOf("ABED"));
+        System.out.println(solver.scoreOf("ABDUCT"));
+        System.out.println(solver.scoreOf("ABERRATE"));
+        System.out.println(solver.scoreOf("ABHORRENT"));
     }
 
 }
