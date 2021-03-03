d5153f3757c7516bd173e27380ab8b0a0af0d950
Mark <shuyumark@gmail.com>
2021-03-03 23:17:30 +0800

    union find

---
+++ src/main/java/com/github/ztmark/basic/UF.java
@@ -0,0 +1,48 @@
+package com.github.ztmark.basic;
+
+public class UF {
+    private final int[] id;
+    private final int[] size;
+    private int count;
+
+    public UF(int n) {
+        id = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            size[i] = 1;
+        }
+        count++;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int i = find(p);
+        int j = find(q);
+        if (i == j) {
+            return;
+        }
+        if (size[i] < size[j]) {
+            id[i] = j;
+            size[j] += size[i];
+        } else {
+            id[j] = i;
+            size[i] += size[j];
+        }
+        count--;
+    }
+
+    public int count() {
+        return count;
+    }
+}
